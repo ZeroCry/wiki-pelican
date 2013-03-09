1007d55fefa2069d02d2c84cc3e713aa1a135fcc
Ashish Dubey <ashish.dubey91@gmail.com>
2013-03-09 15:24:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the home for the tools we use to generate OSDC's Github pages from its markdown content.
+
+The content lives in [wiki-content](https://github.com/osdc/wiki-content).
---
+++ settings.py
@@ -0,0 +1,9 @@
+# Basic Pelican settings file
+import os
+
+AUTHOR = "OSDC"
+
+# Absolute path to wiki-content directory
+PATH = os.path.join(os.path.dirname(__file__), '..', 'wiki-content')
+
+SITEURL = 'http://osdc.github.com/wiki'
